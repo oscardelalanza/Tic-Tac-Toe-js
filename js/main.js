704e330184c555ca7b59e6edddd48b5402ba35d1
Oscar De La Lanza <oscardelalanza@gmail.com>
2019-12-20 10:52:39 -0800

    rendering the board (disabled)

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 const gameBoard = (() => {
     const board = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    const empty = ['o', 'x', 'o', 'x', 'x', 'o', 'o', 'x', 'o'];
 
     const displayBoard = () => {
         const container = document.getElementById('board-container');
@@ -7,9 +8,11 @@ const gameBoard = (() => {
         for (let i = 0; i < 9; i++) {
             const button = document.createElement('button');
             button.id = String(i);
-            button.innerText = String(i + 1);
+            button.innerText = String(empty[i]);
+            button.disabled = true;
             container.appendChild(button);
         }
+
     };
 
     return {
@@ -25,3 +28,5 @@ const playerFactory = (name, token) => {
 };
 
 const gameControl = (() => {})();
+
+gameBoard.displayBoard();

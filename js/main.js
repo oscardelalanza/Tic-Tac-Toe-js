8c17e97757d086a4206987ed24a612af53714f2c
Oscar De La Lanza <oscardelalanza@gmail.com>
2019-12-20 14:38:11 -0800

    board displayed

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 const gameBoard = (() => {
-    const board = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-    const empty = ['o', 'x', 'o', 'x', 'x', 'o', 'o', 'x', 'o'];
+    const board = ['o', 'x', 'o', 'x', 'x', 'o', 'o', 'x', 'o'];
 
     const displayBoard = () => {
         const container = document.getElementById('board-container');
@@ -8,11 +7,10 @@ const gameBoard = (() => {
         for (let i = 0; i < 9; i++) {
             const button = document.createElement('button');
             button.id = String(i);
-            button.innerText = String(empty[i]);
+            button.innerText = String(board[i]);
             button.disabled = true;
             container.appendChild(button);
         }
-
     };
 
     return {

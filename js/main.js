57c5a6b83fef47bdd7374d8737614097e348af18
Oscar De La Lanza <oscardelalanza@gmail.com>
2019-12-31 02:53:07 -0800

    button event listener changed for container event listener:

--- js/main.js
+++ js/main.js
@@ -9,19 +9,15 @@ const gameBoard = (() => {
         }
     };
 
-    const displayBoard = (actualPlayer) => {
+    const displayBoard = () => {
         const container = document.getElementById('board-container');
 
         for (let i = 0; i < 9; i++) {
             const button = document.createElement('button');
             button.id = String(i);
+            button.name = 'btn';
             button.innerText = String(board[i]);
             container.appendChild(button);
-
-            button.addEventListener('click', () => {
-               setToken(actualPlayer, button.id);
-               button.disabled = true;
-            });
         }
     };
 
@@ -39,9 +35,27 @@ const playerFactory = (name, token) => {
     };
 };
 
-const gameControl = (() => {})();
+const gameControl = (() => {
+    const player1 = playerFactory('player 1', 'X');
+    const player2 = playerFactory('player 2', 'O');
+    let actualPlayer = player1;
+    let board = document.getElementById('board-container');
 
-gameBoard.displayBoard(1);
+    const play = () => {
+        gameBoard.displayBoard();
+        board.addEventListener('click', (e) => {
+            if (e.target.name === 'btn') {
+                // TODO: working on setting token
+            }
+        })
+    };
+
+    return {
+        play
+    }
+})();
+
+gameControl.play();
 
 // TODO: toggle actual player for switch between movements
 // TODO: create game control module to handle game functionality
